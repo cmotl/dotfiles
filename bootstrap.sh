2e9b7e38534e5e6bd42872f48bb9536e569076c4
Christopher Motl <cmotl@cmotl.com>
2026-06-01 19:49:28 -0500

    Fix nix experimental features quoting; use bash array

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 DOTFILES="$HOME/dotfiles"
-NIX="nix --extra-experimental-features 'nix-command flakes'"
+NIX=(nix --extra-experimental-features "nix-command flakes")
 
 # Remove any real files that stow would conflict with
 for f in "$HOME/.config/nix/nix.conf"; do
@@ -12,10 +12,10 @@ for f in "$HOME/.config/nix/nix.conf"; do
 done
 
 # Link all stow packages
-$NIX shell nixpkgs#stow --command \
+"${NIX[@]}" shell nixpkgs#stow --command \
   stow --target="$HOME" -d "$DOTFILES" bash tmux vim nvim nix
 
 # Activate home-manager (installs all packages declaratively)
-$NIX run home-manager/master -- switch --flake "$DOTFILES/home-manager#cmotl"
+"${NIX[@]}" run home-manager/master -- switch --flake "$DOTFILES/home-manager#cmotl"
 
 echo "Done. Open a new shell to pick up the new environment."
