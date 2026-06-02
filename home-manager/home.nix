855688c43946ff0644ea411f98b1aafac9c8dad2
Christopher Motl <cmotl@cmotl.com>
2026-06-01 19:55:13 -0500

    Add Hack Nerd Font via home-manager

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
   home.stateVersion = "24.11";
 
   home.packages = with pkgs; [
@@ -6,7 +6,14 @@
     tmux
     tree-sitter
     stow
+    nerd-fonts.hack
   ];
+
+  # Symlink Hack Nerd Font into ~/Library/Fonts (macOS)
+  home.activation.installFonts = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    mkdir -p "$HOME/Library/Fonts"
+    find ${pkgs.nerd-fonts.hack}/share/fonts -name "*.ttf" -exec ln -sf {} "$HOME/Library/Fonts/" \;
+  '';
 
   programs.direnv = {
     enable = true;
