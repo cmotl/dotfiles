ee41e2fcec868ed3364a4a653e16cc3ab46745af
Christopher Motl <cmotl@cmotl.com>
2026-06-01 19:17:16 -0500

    Migrate to stow + home-manager
    - Reorganize dotfiles into stow packages (bash, tmux, vim)
    - Add home-manager flake managing neovim, tmux, tree-sitter, stow, direnv

---
+++ home-manager/flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "cmotl home-manager configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }: {
+    homeConfigurations."cmotl" = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+      modules = [ ./home.nix ];
+    };
+  };
+}
---
+++ home-manager/home.nix
@@ -0,0 +1,19 @@
+{ pkgs, ... }: {
+  home.username = "cmotl";
+  home.homeDirectory = "/Users/cmotl";
+  home.stateVersion = "24.11";
+
+  home.packages = with pkgs; [
+    neovim
+    tmux
+    tree-sitter
+    stow
+  ];
+
+  programs.direnv = {
+    enable = true;
+    enableBashIntegration = true;
+  };
+
+  programs.home-manager.enable = true;
+}
